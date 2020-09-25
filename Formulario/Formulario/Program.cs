7fe053646502cb4922c342583881d69489e394e8
@1juan3arvizu <naujmanuel1311@gmail.com>
2020-09-25 13:12:57 -0500

    Progress abour forms
    Co-Authored-By: ZulymRossVel <71784771+ZulymRossVel@users.noreply.github.com>
    Co-Authored-By: IsraelGil019 <71783505+IsraelGil019@users.noreply.github.com>
    Co-Authored-By: asalmero <71348959+asalmero@users.noreply.github.com>

---
+++ Formulario/Formulario/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Formulario
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            String Name, Number, Email, Job;
+            int Age;
+            Console.WriteLine("What´s your name?");
+            Name = Console.ReadLine();
+            Console.WriteLine("How old are you?");
+            Age = int.Parse(Console.ReadLine());
+            //age=Convert.Toint32(Console.ReadLine();
+
+            Console.WriteLine("Your name is: {0}", Name);
+            Console.WriteLine("Your age is: {0}", Age);
+            if (Age < 17)
+            {
+                Console.WriteLine("Your age is not accepted, registering is not able. ")
+            }
+            else
+            {
+                Console.WriteLine("Registering SUCCESFULLY... ")
+            }
+        }
+    }
+}
